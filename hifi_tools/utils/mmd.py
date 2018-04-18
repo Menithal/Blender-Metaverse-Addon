0633838326c1611b8b1f907a8b6e996e175f653d
Menithal <menithal@menithal.com>
2018-04-19 02:41:52 +0300

    Improved Vertex info cleanup

--- hifi_tools/utils/mmd.py
+++ hifi_tools/utils/mmd.py
@@ -62,7 +62,8 @@ contains_to_remove = [
     "mmd_vertex",
     "Node",
     "Center",
-    "Control"
+    "Control",
+    "Return"
 
 ]
 
@@ -508,17 +509,28 @@ def fix_vertex_groups(obj):
 
     remove_list = []
     for vertex_group in vertex_groups:  
-        if "Waist" in vertex_group.name:
-            mix_weights("Hips", "Waist")
-            remove_list.append(vertex_group)
-        if "Head2" in vertex_group.name:
-            mix_weights("Head", "Head2")
-            remove_list.append(vertex_group)
         if "IK" in vertex_group.name:
             remove_list.append(vertex_group)
-        if "Eyes" == vertex_group.name:
+
+            
+        if "RightEyeReturn" in vertex_group.name:
+            mix_weights("RightEye", "RightEyeReturn")
+            remove_list.append(vertex_group)
+            
+        elif "LeftEyeReturn" in vertex_group.name:
+            mix_weights("LeftEye", "LeftEyeReturn")
+            remove_list.append(vertex_group)
+        elif "Eyes" == vertex_group.name:
             mix_weights("Head", "Eyes")
             remove_list.append(vertex_group)
+            
+        elif "Waist" in vertex_group.name:
+            mix_weights("Hips", "Waist")
+            remove_list.append(vertex_group)
+        elif "Head2" in vertex_group.name:
+            mix_weights("Head", "Head2")
+            remove_list.append(vertex_group)
+            
         elif "ArmTwist" in vertex_group.name:
             root = re.sub(arm_re, "Arm", vertex_group.name)
             parent = vertex_groups.get(root)
@@ -557,34 +569,63 @@ def fix_vertex_groups(obj):
 
 def clean_unused_vertex_groups(obj):
     ## This part is generic:
+    
+    bpy.ops.object.mode_set(mode='OBJECT')
     vertex_groups = copy.copy ( obj.vertex_groups.values() )
+    
     empty_vertex = []
 
     for vertex in obj.data.vertices:
-        
         if len(vertex.groups) < 0:
             empty_vertex.append(vertex)
-            continue
 
-        for group in vertex.groups:
-            if group in vertex_groups:
-                vertex_groups.remove(group)
-                print("  - Removing ", group)
+        index = vertex.index
+        
+        has_use = []
+        for group in vertex_groups:
+            try:
+                obj.vertex_groups[group.name].weight(index)
+                if group not in has_use:
+                    has_use.append(group)
+            except RuntimeError:
+                pass
+
+        for used in has_use:
+            vertex_groups.remove(used)
     
-    print( " Found ", len(vertex_groups), " without weights, cleaning up")
-    print( " Removing Unused Bones")
+    
+    print( " Removing Unused Bones", obj.parent.type)
+
+    parent = obj.parent
     _to_remove_bones = []
-    if obj.parent.type is "ARMATURE":
-        for edit_bone in obj.parent.data.edit_bones:
-            if edit_bone.name in vertex_groups:
-                print( "  - Removing Bone", edit_bone.name)
+    if parent.type == "ARMATURE":
+        
+        obj.select = False
+
+        bpy.context.scene.objects.active = parent
+        parent.select = True
+        mapped = [(x.name) for x in vertex_groups]
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        print(" Iterating edit bones", len(parent.data.edit_bones))
+        for edit_bone in parent.data.edit_bones:
+            if edit_bone.name in mapped and edit_bone.name != "HeadTop":
+                print( "  - Removing Unused Bone", edit_bone.name)
                 _to_remove_bones.append(edit_bone)
 
         for bone_to_remove in _to_remove_bones:
-            obj.parent.data.edit_bones.remove(bone_to_remove)
+            parent.data.edit_bones.remove(bone_to_remove)
 
+
+    print( " Found ", len(vertex_groups), " without weights, cleaning up", obj.parent)
+    for group in vertex_groups:
+        obj.vertex_groups.remove(group)
+        print("  - Removing Vertex Groups ", group)
+
+    bpy.ops.object.mode_set(mode='OBJECT')
     print (" Found", len(empty_vertex), " Unused Vertices")
 
+    bpy.context.scene.objects.active = obj
 
 def clean_mesh(Translator, obj):
     bpy.ops.object.mode_set(mode='OBJECT')
