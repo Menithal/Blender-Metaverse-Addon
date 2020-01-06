a1477ae7f624d95aaf7cb783cab8006860edd6ee
Menithal <menithal@norteclabs.com>
2020-01-06 11:39:14 +0200

    Updated Packaging

--- package.py
+++ package.py
@@ -5,13 +5,13 @@ import zipfile, os, re
 
 ## RUN TESTS PRIOR
 
-script_dir = os.path.join("hifi_tools")
+script_dir = os.path.join("metaverse_tools")
 m = None
 
 with open(os.path.join(script_dir,"__init__.py")) as vs_init:
 	m = re.search("\"version\": \((.*)?\)\,",vs_init.read(),re.MULTILINE)
 
-zip = zipfile.ZipFile(os.path.join("..","hifi_tools_{}.zip".format(m.group(1).replace(", ",".").replace(".0.0",".0"))),'w',zipfile.ZIP_BZIP2)
+zip = zipfile.ZipFile(os.path.join("..","metaverse_tools_{}.zip".format(m.group(1).replace(", ",".").replace(".0.0",".0"))),'w',zipfile.ZIP_BZIP2)
 
 for path, dirnames, filenames in os.walk(script_dir):
 	if path.endswith("__pycache__"): continue
